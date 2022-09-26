61f54ca2d2e1f4c36d10a6ffab16d449f710fc5f
ohshinyeop <ohshinyeop@gmail.com>
2022-09-27 08:43:02 +0900

    first

--- webview/ViewController.swift
+++ webview/ViewController.swift
@@ -1,19 +1,72 @@
-//
-//  ViewController.swift
-//  webview
-//
-//  Created by SHIN-YEOP OH on 2022/09/22.
-//
-
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,WKUIDelegate,WKNavigationDelegate{
+
+    
+    @IBOutlet weak var webView: WKWebView!
+    var popupView: WKWebView?
+    override func loadView() {
+            super.loadView()
+            
+            webView = WKWebView(frame: self.view.frame)
+            webView.uiDelegate = self
+            webView.navigationDelegate = self
+            self.view = self.webView
+
+
+        }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        let url = URL(string: "http://192.168.4.7:3000")
+        let request = URLRequest(url: url!)
+        //self.webView?.allowsBackForwardNavigationGestures = true  //뒤로가기 제스쳐 허용
+        webView.configuration.defaultWebpagePreferences.allowsContentJavaScript = true
+        webView.configuration.preferences.javaScriptCanOpenWindowsAutomatically = true
+
+        webView.load(request)
+
     }
+   
 
 
+    
+    override func didReceiveMemoryWarning() { super.didReceiveMemoryWarning() } //모달창 닫힐때 앱 종료현상 방지.
+    
+    //alert 처리
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String,
+                 initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void){
+        let alertController = UIAlertController(title: "", message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: { (action) in completionHandler() }))
+        self.present(alertController, animated: true, completion: nil) }
+
+    //confirm 처리
+    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
+        let alertController = UIAlertController(title: "", message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "취소", style: .default, handler: { (action) in completionHandler(false) }))
+        alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: { (action) in completionHandler(true) }))
+        self.present(alertController, animated: true, completion: nil) }
+    
+    // href="_blank" 처리
+    func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+        
+        print("팝업 이벤트 캐치 성공")
+
+        popupView = WKWebView(frame: UIScreen.main.bounds, configuration: configuration)
+        popupView?.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        popupView?.uiDelegate = self
+        
+        view.addSubview(popupView!)
+        
+        return popupView
+    }
+    
+    func webViewDidClose(_ webView: WKWebView) {
+        if webView == popupView {
+            popupView?.removeFromSuperview()
+            popupView = nil
+        }
+    }
 }
-
